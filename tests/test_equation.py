eaabc202db4eabf21854cb2f6b73e560d75a9aa8
Mohamed-Moumni <mmoumni@student.1337.ma>
2024-01-20 15:26:56 +0100

    Fix: Update equation tests

--- tests/test_equation.py
+++ tests/test_equation.py
@@ -1,5 +1,5 @@
 import pytest
-from ..src.equation import Equation
+from src.equation import Equation
 
 """
     This file contains test for equation class
@@ -19,14 +19,14 @@ class TestEquation:
         _terms = [{"coeff": 1.0, "degree": 1}, {"coeff": 1.0, "degree": 0}]
         _equation = Equation(_degree, _terms)
         _solution = _equation.solve_one_degree_equation()
-        assert _solution.solution == (-1.0)
+        assert _solution['solution'] == (-1.0)
 
     def test_solve_one_degree_equation_equation2(self):
         _degree = 1
         _terms = [{"coeff": 0.5, "degree": 1}, {"coeff": 4.0, "degree": 0}]
         _equation = Equation(_degree, _terms)
         _solution = _equation.solve_one_degree_equation()
-        assert _solution.solution == (8.0)
+        assert _solution['solution'] == (-8.0)
 
 
     def test_solve_one_degree_equation_equation3(self):
@@ -34,19 +34,19 @@ class TestEquation:
         _terms = [{"coeff": 5.0, "degree": 1}]
         _equation = Equation(_degree, _terms)
         _solution = _equation.solve_one_degree_equation()
-        assert _solution.solution == ()
+        assert _solution['solution'] == ()
 
     def test_solve_one_degree_equation_equation4(self):
         _degree = 1
         _terms = [{"coeff": -5.0, "degree": 1}, {"coeff": -5.0, "degree": 0}]
         _equation = Equation(_degree, _terms)
         _solution = _equation.solve_one_degree_equation()
-        assert _solution.solution == (-1.0)
+        assert _solution['solution'] == (-1.0)
 
     def test_solve_one_degree_equation_equation5(self):
         _degree = 1
         _terms = [{"coeff": 2.0, "degree": 1}, {"coeff": -55.0, "degree": 0}]
         _equation = Equation(_degree, _terms)
         _solution = _equation.solve_one_degree_equation()
-        assert _solution.solution == (
+        assert _solution['solution'] == (
             round(55.0/2.0, 2))
