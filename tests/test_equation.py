babc11ee9ee5f25be2b7ca6c37b95ab80ec9f267
Mohamed-Moumni <mmoumni@student.1337.ma>
2024-01-20 14:51:16 +0100

    Fix: update test_equation file

--- tests/test_equation.py
+++ tests/test_equation.py
@@ -16,37 +16,37 @@ from ..src.equation import Equation
 class TestEquation:
     def test_solve_one_degree_equation_equation1(self):
         _degree = 1
-        _terms = dict({"coeff": 1.0, "degree": 1}, {"coeff": 1.0, "degree": 0})
+        _terms = [{"coeff": 1.0, "degree": 1}, {"coeff": 1.0, "degree": 0}]
         _equation = Equation(_degree, _terms)
         _solution = _equation.solve_one_degree_equation()
-        assert _solution.solution == (-1.0) and _solution.degree == 0
+        assert _solution.solution == (-1.0)
 
     def test_solve_one_degree_equation_equation2(self):
         _degree = 1
-        _terms = dict({"coeff": 0.5, "degree": 1}, {"coeff": 4.0, "degree": 0})
+        _terms = [{"coeff": 0.5, "degree": 1}, {"coeff": 4.0, "degree": 0}]
         _equation = Equation(_degree, _terms)
         _solution = _equation.solve_one_degree_equation()
-        assert _solution.solution == (8.0) and _solution.degree == 0
+        assert _solution.solution == (8.0)
 
 
     def test_solve_one_degree_equation_equation3(self):
         _degree = 1
-        _terms = dict({"coeff": 5.0, "degree": 1})
+        _terms = [{"coeff": 5.0, "degree": 1}]
         _equation = Equation(_degree, _terms)
         _solution = _equation.solve_one_degree_equation()
-        assert _solution.solution == () and _solution.degree == 0
+        assert _solution.solution == ()
 
     def test_solve_one_degree_equation_equation4(self):
         _degree = 1
-        _terms = dict({"coeff": -5.0, "degree": 1}, {"coeff": -5.0, "degree": 0})
+        _terms = [{"coeff": -5.0, "degree": 1}, {"coeff": -5.0, "degree": 0}]
         _equation = Equation(_degree, _terms)
         _solution = _equation.solve_one_degree_equation()
-        assert _solution.solution == (-1.0) and _solution.degree == 0
+        assert _solution.solution == (-1.0)
 
     def test_solve_one_degree_equation_equation5(self):
         _degree = 1
-        _terms = dict({"coeff": 2.0, "degree": 1}, {"coeff": -55.0, "degree": 0})
+        _terms = [{"coeff": 2.0, "degree": 1}, {"coeff": -55.0, "degree": 0}]
         _equation = Equation(_degree, _terms)
         _solution = _equation.solve_one_degree_equation()
         assert _solution.solution == (
-            round(55.0/2.0, 2)) and _solution.degree == 0
+            round(55.0/2.0, 2))
