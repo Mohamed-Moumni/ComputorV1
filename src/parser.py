1cd0b2b87916f706a87331aacdc8edf29cfb07d5
Mohamed-Moumni <mmoumni@student.1337.ma>
2024-07-23 10:08:37 +0100

    Test: add equal Sign tests for Parser Class

--- src/parser.py
+++ src/parser.py
@@ -7,43 +7,57 @@ class Parser:
         self.equation = _equation
         self.remove_spaces()
 
+    @staticmethod
+    def parser_error(index: int, side: int, equation_side: str) -> None:
+        sides: List[str] = ["Left", "right"]
+        print(f"Encounter an error at the {sides[side]}, At index {index}")
+        print(equation_side)
+        location_error: str = (
+            "_" * (index - 1) + "^" + (len(equation_side) - index + 1) * "_"
+        )
+        print(location_error)
+
     def start(self) -> List[Dict[str, float]]:
         terms: List[Dict[str, float]] = []
         equal_found: int = self.equation.count("=")
+
         if equal_found == 0:
             raise SyntaxError("No Equal (=) sign found in The Equation")
         if equal_found > 1:
             raise SyntaxError("There are more than one equal Sign (=)")
+
         parsed_equation: List[str] = self.equation.split("=")
         left_side: str = parsed_equation[0]
         right_side: str = parsed_equation[1]
         if left_side != "0":
-            terms.append(Parser.parse_equation_side(left_side))
+            for term in Parser.parse_equation_side(left_side, 0):
+                terms.append(term)
         if right_side != "0":
-            terms.append(Parser.parse_equation_side(right_side))
+            for term in Parser.parse_equation_side(right_side, 1):
+                terms.append(term)
         return terms
 
     def remove_spaces(self):
         self.equation = re.sub(r"\s+", "", self.equation)
 
     @staticmethod
-    def get_term(index: int, equation_side: str) -> Dict[str, float]:
+    def get_term(index: int, equation_side: str, side: int) -> Dict[str, float]:
         print(f"Index: {index} -- Equation Side: {equation_side[index:]}")
         equation_side_len: int = len(equation_side)
         term: Dict[str, float] = {}
-        coeff, index = Parser.get_coeff(index, equation_side)
+        coeff, index = Parser.get_coeff(index, equation_side, side)
         print(f"Coefficient: {coeff}")
         term["coeff"] = coeff
         term["degree"] = 0
         if index < equation_side_len and equation_side[index] == "*":
-            index = Parser.get_X_term(index + 1, equation_side)
-            degree, index = Parser.get_degree(index, equation_side)
+            index = Parser.get_X_term(index + 1, equation_side, side)
+            degree, index = Parser.get_degree(index, equation_side, side)
             print(f"Degree: {degree}")
             term["degree"] = degree
         return term, index
 
     @staticmethod
-    def get_coeff(index: int, equation_side: str) -> float:
+    def get_coeff(index: int, equation_side: str, side: int) -> float:
         print(f"Index: {index} -- Coeff Side: {equation_side[index:]}")
         start: int = index
         end: int = index
@@ -54,6 +68,7 @@ class Parser:
                 if not float_point:
                     float_point = True
                 else:
+                    Parser.parser_error(end, side, equation_side)
                     raise SyntaxError("Invalid Coefficient format")
             elif not equation_side[end].isdigit():
                 break
@@ -63,19 +78,21 @@ class Parser:
         return float(equation_side[start:end]), end
 
     @staticmethod
-    def get_X_term(index: int, equation_side: str) -> None:
+    def get_X_term(index: int, equation_side: str, side: int) -> None:
         print(f"Index: {index} -- X Term Side: {equation_side[index:]}")
         equation_side_len: int = len(equation_side)
         if index < equation_side_len and equation_side[index] != "X":
+            Parser.parser_error(index, side, equation_side)
             raise SyntaxError("Term Syntax Error (X)")
         index += 1
         if index < equation_side_len and equation_side[index] != "^":
+            Parser.parser_error(index, side, equation_side)
             raise SyntaxError("Term Syntax Error (^)")
         index += 1
         return index
 
     @staticmethod
-    def get_degree(index: int, equation_side: str) -> int:
+    def get_degree(index: int, equation_side: str, side: int) -> int:
         print(f"Index: {index} -- Degree Side: {equation_side[index:]}")
         equation_side_len: int = len(equation_side)
         start: int = index
@@ -91,7 +108,7 @@ class Parser:
         return degree, end
 
     @staticmethod
-    def parse_equation_side(equation_side: str) -> List[Dict[str, float]]:
+    def parse_equation_side(equation_side: str, side: int) -> List[Dict[str, float]]:
         terms: List[Dict[str, float]] = []
         first_term: bool = True
         index: int = 0
@@ -102,8 +119,9 @@ class Parser:
                 equation_side = "+" + equation_side
                 first_term = False
             if equation_side[index] not in ["+", "-"]:
+                Parser.parser_error(index, side, equation_side)
                 raise SyntaxError("Error in your Equation Syntax")
-            term, current_index = Parser.get_term(index + 1, equation_side)
+            term, current_index = Parser.get_term(index + 1, equation_side, side)
             print(f"TERM: {term}")
             terms.append(term)
             index = current_index
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -32,18 +32,18 @@ class TestParser:
 
     def test_get_X_term(self):
         parser = Parser("")
-        assert parser.get_X_term(2, "2*X^4") == 4
-        assert parser.get_X_term(0, "X^") == 2
-        assert parser.get_X_term(2, "3*X^3") == 4
+        assert parser.get_X_term(2, "2*X^4", 0) == 4
+        assert parser.get_X_term(0, "X^", 0) == 2
+        assert parser.get_X_term(2, "3*X^3", 0) == 4
 
     def test_get_degree(self):
         parser = Parser("")
-        assert parser.get_degree(0, "2hhhh") == (2, 1)
-        assert parser.get_degree(4, "2*X^3") == (3, 5)
+        assert parser.get_degree(0, "2hhhh", 0) == (2, 1)
+        assert parser.get_degree(4, "2*X^3", 0) == (3, 5)
         with pytest.raises(SyntaxError):
-            parser.get_degree(4, "2*X^4") == (3, 5)
+            parser.get_degree(4, "2*X^4", 0) == (3, 5)
         with pytest.raises(SyntaxError):
-            parser.get_degree(4, "2*X^-3") == (3, 5)
+            parser.get_degree(4, "2*X^-3", 0) == (3, 5)
 
     def test_start_1(self):
         parser = Parser("2 * X ^ 2 + 3 * X ^ 1 - 2 = 0")
@@ -54,20 +54,69 @@ class TestParser:
         ]
         terms = parser.start()
         assert terms == expected_terms
-    
-    # def test_start_2(self):
-    #     parser = Parser("2 * X ^ 2 + 3 * X ^ 1 - 2 = 2 * X ^ 2 + 3.5 + 1 * X ^ 0")
-    #     expected_terms = [
-    #         {"coeff": 2.0, "degree": 2},
-    #         {"coeff": 3.0, "degree": 1},
-    #         {"coeff": 2.0, "degree": 0},
-    #         {"coeff": 2.0, "degree": 2},
-    #         {"coeff": 3.5, "degree": 0},
-    #         {"coeff": 1.0, "degree": 0},
-    #     ]
-    #     terms = parser.start()
-    #     print(f"Terms: {terms}")
-    #     print(f"Expected Terms: {expected_terms}")
-    #     assert terms == expected_terms
-        
-        
+
+    def test_equal_sign(self):
+        parser = Parser("=")
+        with pytest.raises(SyntaxError):
+            parser.start()
+
+        parser = Parser("2 = 2")
+        expected_terms = [
+            {"coeff": 2.0, "degree": 0},
+            {"coeff": 2.0, "degree": 0},
+        ]
+        terms = parser.start()
+        assert sorted(terms, key=lambda d: d["degree"]) == sorted(
+            expected_terms, key=lambda d: d["degree"]
+        )
+
+        parser = Parser("2 = 0")
+        expected_terms = [
+            {"coeff": 2.0, "degree": 0},
+        ]
+        terms = parser.start()
+        assert sorted(terms, key=lambda d: d["degree"]) == sorted(
+            expected_terms, key=lambda d: d["degree"]
+        )
+
+        parser = Parser("2 == 0")
+        with pytest.raises(SyntaxError):
+            parser.start()
+
+        parser = Parser("2 + 3 + 4")
+        with pytest.raises(SyntaxError):
+            parser.start()
+
+        parser = Parser("2 === 4")
+        with pytest.raises(SyntaxError):
+            parser.start()
+
+        parser = Parser("3 - 2 = 3")
+        expected_terms = [
+            {"coeff": 3.0, "degree": 0},
+            {"coeff": 2.0, "degree": 0},
+            {"coeff": 3.0, "degree": 0},
+        ]
+        terms = parser.start()
+        assert sorted(terms, key=lambda d: d["degree"]) == sorted(
+            expected_terms, key=lambda d: d["degree"]
+        )
+
+        parser = Parser("2 = 3 = 4")
+        with pytest.raises(SyntaxError):
+            parser.start()
+
+    # 0 degree Tests:
+    # X^0 + 1 = 0 error
+    # 1*X^0 + 1 = 0 good
+    # 2 * X ^ 1 + 1 +2+3+4= 0 good
+    # 2X^1 + 14 -22 = 0 error
+    # 1 degree Tests:
+    # X^1 + 2 = 0 error
+    # 2 * X ^ 1 - 4 * X ^ 0 + 25 = 0
+    # X + 2 = 0 error
+    # 2 degree Tests:
+    # -
+    # -
+    # -
+    # -
